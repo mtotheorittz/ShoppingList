30bac475f2f5342b51a4d293935bcab34340366c
mtotheorittz <54841160+mtotheorittz@users.noreply.github.com>
2019-10-31 11:28:14 +0100

    Add files via upload

--- shoppingList.js
+++ shoppingList.js
@@ -1,92 +1,90 @@
 
 //gibt Liste zurück
 function getlist(id) {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("Items").innerHTML =
-        this.responseText;
-      }
-    };
-    xhttp.open("GET", "https://shopping-lists-api.herokuapp.com/api/v1/lists/" + id, true);
-    xhttp.send();
-  }
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("Items").innerHTML =
+      this.responseText;
+    }
+  };
+  xhttp.open("GET", "https://shopping-lists-api.herokuapp.com/api/v1/lists/" + id, true);
+  xhttp.send();
+}
 
 //add
-  function additem() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("Items").innerHTML =
-        this.responseText;
-      }
-    };
-    xhttp.open("POST", "https://shopping-lists-api.herokuapp.com/api/v1/lists/5d9c7b9ee2d16f00178a5c6b/items", true);
-    xhttp.send();
-  }
+function additem(input) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById(input).innerHTML =
+      this.responseText;
+    }
+  };
+  xhttp.open("POST", "https://shopping-lists-api.herokuapp.com/api/v1/lists/"+id+"/items", true);
+  xhttp.send();
+}
 
 //remove item
 function removeitem() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("Items").innerHTML =
-        this.responseText;
-      }
-    };
-    xhttp.open("DELETE", "https://shopping-lists-api.herokuapp.com/api/v1/lists/5d9c7b9ee2d16f00178a5c6b/items/ITEMID???", true);
-    xhttp.send();
-  }
-
-  //update item
-  function updateitem() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("Items").innerHTML =
-        this.responseText;
-      }
-    };
-    xhttp.open("PUT", "https://shopping-lists-api.herokuapp.com/api/v1/lists/5d9c7b9ee2d16f00178a5c6b/items/ITEMID???", true);
-    xhttp.send();
-  }
-
-  
-var itemid;
-
-var id;
-
-var optionListe;
-
-welcheListe();
-
-function welcheListe() {
-
-  switch (optionListe) {
-
-    case "Liste1":
-
-      this.id = "5d9c7b9ee2d16f00178a5c6b";
-
-      break;
-
-    case "Liste2":
-
-      this.id = "5db00803655dfa001785c029";
-
-      break;
-
-    default:
-
-      this.id = "5db00809655dfa001785c02a";
-
-  }
-
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("Items").innerHTML =
+      this.responseText;
+    }
+  };
+  xhttp.open("DELETE", "https://shopping-lists-api.herokuapp.com/api/v1/lists/"+id+"/items/ITEMID???", true);
+  xhttp.send();
 }
 
-//switch case durch getElementById unnötig
+//update item
+function updateitem() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("Items").innerHTML =
+      this.responseText;
+    }
+  };
+  xhttp.open("PUT", "https://shopping-lists-api.herokuapp.com/api/v1/lists/"+id+"/items/ITEMID???", true);
+  xhttp.send();
+}
+
+
 
 document.getElementById("listSelect").addEventListener('change', function(event) {
-  console.log(event.target.value); // Get ID of list
-  getlist(event.target.value);
+console.log(event.target.value); // Get ID of list
+getlist(event.target.value);
 })
+
+
+//Checkbox
+document.addEventListener('change', function (e) {
+
+    if (e.target.name === 'checkItems') {
+      if (e.target.checked) {
+        e.target.parentNode.className = 'selected';
+      } else {
+        e.target.parentNode.className = '';
+      }
+    }
+});
+
+
+// Get the input field
+var input = document.getElementById("myInput");
+
+// Execute a function when the user releases a key on the keyboard
+input.addEventListener("keydown", function(event) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("myBtn").click();
+  }
+});
+
+
+
